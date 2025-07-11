842f7af926d025b792a28268e212d6f1d68a6cac
Akash <akash.krishnan.046@gmail.com>
2025-07-11 11:11:06 +0530

    feat: remove deprecated files and configurations to streamline project structure

--- rago/scripts/loadDb.ts
+++
@@ -1,5 +0,0 @@
-import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer";
-import { DataAPIClient } from "@datastax/astra-db-ts";
-import OpenAI from "openai";
-import "dotenv/config";
-import { RecursiveCharacterTextSplitter } from "@langchain/text_splitter";
